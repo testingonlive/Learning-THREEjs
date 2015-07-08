518f2ed0a905d485c0d498f4099d9ab71d38d6f1
jonny5 <e_t@mail.com>
2015-07-08 20:28:26 +0000

    commiting few bits

---
+++ worked-examples/chapter-3/spot-light.js
@@ -0,0 +1,146 @@
+function init() {
+    
+    var stats = initStats();
+    
+    var scene = new THREE.Scene();
+    
+    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1e3 );
+    
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( new THREE.Color( 0xEEEEEE, 1 ) );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMapEnabled = true;
+    renderer.shadowMapType = THREE.PCFShadow;
+    
+    
+    var planeGeometry = new THREE.PlaneGeometry( 60, 20, 1, 1 );
+    var planeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xffffff
+    });
+    var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+    plane.receiveShadow = true;
+    
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    
+    scene.add( plane );
+    
+    var cubeGeometry = new THREE.BoxGeometry( 4, 4, 4 );
+    var cubeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xff3333
+    });
+    var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+    cube.castShadow = true;
+    
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+    
+    scene.add( cube );
+    
+    var sphereGeometry = new THREE.SphereGeometry( 4, 20, 20 );
+    var sphereMaterial = new THREE.MeshLambertMaterial({
+        color: 0x7777ff
+    });
+    var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+    
+    sphere.position.x = 20;
+    sphere.position.y = 0;
+    sphere.position.z = 2;
+    sphere.castShadow = true;
+    
+    scene.add( sphere );
+    
+    camera.position.x = -35;
+    camera.position.y = 30;
+    camera.position.z = 25;
+    camera.lookAt( new THREE.Vector3( 10, 0, 0 ) );
+    
+    var ambiColor = '#1c1c1c';
+    var ambientLight = new THREE.AmbientLight( ambiColor );
+    
+    scene.add( ambientLight );
+    
+    var spotLight0 = new THREE.SpotLight( 0xcccccc );
+    spotLight0.position.set( -40, 30, -10 );
+    spotLight0.castShadow = true;
+    
+    scene.add( spotLight0 );
+    
+    // up to here.
+    // line 108
+    
+    document.getElementById( 'WebGL-output' ).appendChild( renderer.domElement );
+    
+    var step = 0;
+    
+    var controls = new function() {
+        this.rotationSpeed = 0.02;
+        this.bouncingSpeed = 0.03;
+        this.ambientColor = ambiColor;
+        this.disableSpotlight = false;
+    }
+    
+    var gui = new dat.GUI();
+    gui.addColor( controls, 'ambientColor' ).onChange(function( e ) {
+        ambientLight.color = new THREE.Color( e );
+    });
+    gui.add( controls, 'disableSpotlight' ).onChange(function( e ) {
+        spotLight.visible = !e;
+    });
+    
+    render();
+    
+    function render() {
+        stats.update();
+        
+        cube.rotation.x += controls.rotationSpeed;
+        cube.rotation.y += controls.rotationSpeed;
+        cube.rotation.z += controls.rotationSpeed;
+        
+        step += controls.bouncingSpeed;
+        
+        sphere.position.x = 20 + ( 10 * ( Math.cos( step ) ) );
+        sphere.position.y = 2 + ( 10 * Math.abs( Math.sin( step ) ) );
+        
+        requestAnimationFrame( render );
+        renderer.render( scene, camera );
+    }
+    
+    function initStats() {
+        
+        var stats = new Stats();
+        
+        stats.setMode( 0 );
+        
+        stats.domElement.style.postion = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        
+        document.getElementById( 'Stats-output' ).appendChild( stats.domElement );
+        
+        return stats;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
